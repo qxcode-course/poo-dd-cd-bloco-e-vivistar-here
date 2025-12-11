3e521732cf154e23d93c1c276606bca2fddf1bc9
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-12-11 00:40:50 +0000

    quemd era ter 500 de limite

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -5,7 +5,7 @@ class Animal(ABC):
         self.nome = nome
 
     def apresentar_nome(self):
-        return f"Eu sou um(a) {self.nome}!"
+        return f"Eu sou {self.nome}!"
 
     @abstractmethod
 
@@ -26,8 +26,39 @@ class Tartaruga(Animal):
 
     def mover(self):
         return "Move-se nas sombras."
+    
+class Gato(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def fazer_som(self):
+        return "miau"
+
+    def mover(self):
+        return "ataque ferozmente fofo"
+
+class Égua(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def fazer_som(self):
+        return "nirrrr"
+
+    def mover(self):
+        return "pocoto pocoto pocoto"
+
+def apresentar(animal: Animal):
+        print(animal.apresentar_nome())
+        print("Som:", animal.fazer_som())
+        print("Movimento:", animal.mover())
+        print("Tipo:", type(animal).__name__)
+        print("-----------")
 
 t = Tartaruga("Michelangelo")
-print(t.apresentar_nome())
-print(t.fazer_som())
-print(t.mover())
+g = Gato("Vitin")
+e = Égua("Éguinha Pocotó")
+
+lista = [t, g, e]
+
+for animal in lista:
+    apresentar(animal)
