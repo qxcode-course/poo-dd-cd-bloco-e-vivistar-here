1736420b08b230435f77411a72dc9b71e4acb83b
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-12-15 01:03:50 +0000

    dor de cabeçaaa

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -27,8 +27,8 @@ class Veiculo(ABC):
         y = 10 - len(self.getId())
 
         return (
-            f"{'_' * x + self._tipo} :"
-            f"{'_' * y + self.getId()} :"
+            f"{'_' * x + self._tipo} : "
+            f"{'_' * y + self.getId()} : "
             f"{self._horaEntrada}"
         )
 
@@ -46,7 +46,7 @@ class Moto(Veiculo):
     def calcularValor(self, horaSaida: int) -> float:
         tempo = horaSaida - self._horaEntrada
         valor = tempo / 20
-        return 
+        return tempo / 20
         
 class Carro(Veiculo):
     def __init__(self, id: str):
@@ -59,7 +59,7 @@ class Carro(Veiculo):
         if valor < 5:
             return 5
         else:
-            return tempo
+            return valor
 
 class Estacionamento:
     def __init__(self):
@@ -69,8 +69,8 @@ class Estacionamento:
     def procurarVeiculo(self, id: str) -> int:
         for i, veiculo in enumerate(self.__veiculos):
             if veiculo.getId() == id:
-                return id
-            return -1
+                return i
+        return -1
 
     def estacionar(self, veiculo: Veiculo) -> None:
         veiculo.setEntrada(self.__horaAtual)
@@ -83,8 +83,8 @@ class Estacionamento:
             valor = veiculo.calcularValor(self.__horaAtual)
             print(
                 f"{veiculo.getTipo()} chegou {veiculo.getEntrada()}"
-                f"saiu {self.__horaAtual}."
-                f"Pagar R$ {valor:.2f}"
+                f" saiu {self.__horaAtual}."
+                f" Pagar R$ {valor:.2f}"
             )
 
     def sair(self, id: str) -> None:
@@ -93,4 +93,42 @@ class Estacionamento:
     def passarTempo(self, tempo: int) -> None:
         self.__horaAtual += tempo
 
-    def __str__(self) ->
+    def __str__(self) -> str:
+        if len(self.__veiculos) != 0:
+            return f"{"\n".join(str(x) for x in self.__veiculos)}\nHora atual: {self.__horaAtual}"
+        else:
+            return f"Hora atual: {self.__horaAtual}"
+
+def main():
+    estacionamento = Estacionamento()
+
+    while True:
+        line = input()
+        args = line.split(" ")
+        print(f"${line}")
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "show":
+            print(estacionamento)
+        
+        elif args[0] == "tempo":
+            tempo = int(args[1])
+            estacionamento.passarTempo(tempo)
+
+        elif args[0] == "estacionar":
+            if args[1] == "bike":
+                veiculo = Bike(args[2])
+                estacionamento.estacionar(veiculo)
+            elif args[1] == "moto":
+                veiculo = Moto(args[2])
+                estacionamento.estacionar(veiculo)
+            elif args[1] == "carro":
+                veiculo = Carro(args[2])
+                estacionamento.estacionar(veiculo)
+        
+        elif args[0] == "pagar":
+            estacionamento.pagar(args[1])
+
+main()
